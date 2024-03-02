8e66a78a77f69750eb8d0d562dfefaa6ffed161c
Your Name <songkrian2014@gmail.com>
2024-03-03 01:27:46 +0700

    added revise setup packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,8 +13,8 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-uses-react': 'error',   
-    'react/jsx-uses-var': 'error', 
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-var': 'error',
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
